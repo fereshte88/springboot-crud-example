98e298bbd470a38781cbe9bef5721093a960d881
ADMIN <fereshte.rad88@gmail.com>
2024-12-10 22:45:18 +0330

    Add redis config and cache

--- src/main/java/com/springBoot/crud/example/service/Impl/CustomerServiceImpl.java
+++ src/main/java/com/springBoot/crud/example/service/Impl/CustomerServiceImpl.java
@@ -4,27 +4,35 @@ import com.springBoot.crud.example.model.Customer;
 import com.springBoot.crud.example.repositories.CustomerRepository;
 import com.springBoot.crud.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@CacheConfig(cacheNames = "customerCache")
 public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Cacheable(cacheNames = "customers")
     @Override
     public List<Customer> getAll() {
         return customerRepository.findAll();
     }
 
+    @CacheEvict(cacheNames = "customers", allEntries = true)
     @Override
     public Customer add(Customer customer) {
         return customerRepository.save(customer);
     }
 
+    @CacheEvict(cacheNames = "customers", allEntries = true)
     @Override
     public Customer update(Customer customer) {
         Optional<Customer> optCustomer = this.customerRepository.findById(customer.getId());
@@ -36,11 +44,14 @@ public class CustomerServiceImpl implements CustomerService {
         return customerRepository.save(repCustomer);
     }
 
+    @Caching(evict = { @CacheEvict(cacheNames = "customer", key = "#id"),
+            @CacheEvict(cacheNames = "customers", allEntries = true) })
     @Override
     public void delete(long id) {
             customerRepository.deleteById(id);
     }
 
+    @Cacheable(cacheNames = "customer", key = "#id", unless = "#result == null")
     @Override
     public Customer getCustomerById(long id) {
         return customerRepository.findById(id).orElse(null);
